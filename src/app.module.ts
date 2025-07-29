7210f35dde28b05d165d8ae36e73f3e902fbe4b0
islamicelam <islamvelikhanli@Islams-iMac.local>
2025-07-29 16:48:03 +0400

    tasks resource with crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+  ]
 })
 export class AppModule {}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './tasks.entity';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepo: Repository<Task>,
+  ) {}
+
+  create(title: string): Promise<Task> {
+    const task = this.taskRepo.create({ title });
+    return this.taskRepo.save(task);
+  }
+
+  findAll(): Promise<Task[]> {
+    return this.taskRepo.find();
+  }
+
+  findOne(id: number): Promise<Task | null> {
+    return this.taskRepo.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.taskRepo.delete(id);
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    return this.taskRepo.save({ ...updateTaskDto, id });
+  }
+}
