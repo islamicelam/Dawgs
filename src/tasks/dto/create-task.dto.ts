e13e5512ff9b1d61c663eb586efc78e5cd9dd7d7
islamicelam <islamvelikhanli@Islams-iMac.local>
2025-08-08 11:53:04 +0400

    fixes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,3 +1,8 @@
+import { IsString, MinLength, IsNotEmpty } from "class-validator";
+
 export class CreateTaskDto {
+    @IsString()
+    @MinLength(3)
+    @IsNotEmpty()
     title: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @MinLength(3)
+    name: string;
+
+    @IsString()
+    @MinLength(4)
     password: string;
 }
