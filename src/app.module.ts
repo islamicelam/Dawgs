08a803add38733c15df1f838653672421820a2bb
islamicelam <islamovice@gmail.com>
2026-01-20 20:05:16 +0400

    pgdata

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,34 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [__dirname + '/**/*.entity.{ts,js}'],
-      synchronize: true,
+    // ENV configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
+
+    // Database connection
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: true, // ❗ только для локальной разработки
+      }),
+    }),
+
     UsersModule,
     TasksModule,
     AuthModule,
