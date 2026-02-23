ff35a977d7efe09e61befe492cb3340f481e81a3
Islam Velikhanli <islamovice@gmail.com>
2026-02-23 10:21:35 +0400

    services boards and projects

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './boards.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { Repository } from 'typeorm';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+
+  constructor(
+    @InjectRepository(Board)
+    private boardRepo: Repository<Board>,
+  ) {}
+
+  async create(createBoardDto: CreateBoardDto) {
+    const board = this.boardRepo.create(createBoardDto);
+    return await this.boardRepo.save(board);
+  }
+
+  async update(id: number, updateBoardDto: UpdateBoardDto) {
+    return await this.boardRepo.save({ ...updateBoardDto, id });
+  }
+
+  async findOne(id: number) {
+    const board = await this.boardRepo.findOneBy({ id });
+    if (!board) throw new NotFoundException('Board not found');
+    return board;
+  }
+
+  async remove(id: number) {
+    const result = await this.boardRepo.delete(id);
+    if (result.affected === 0) throw new NotFoundException('Board not found');
+  } 
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Project } from './projects.entity';
 import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
 export class ProjectsService {
@@ -12,7 +14,16 @@ export class ProjectsService {
 
   async findOne(id: number) {
     const project = await this.projectRepo.findOneBy({ id });
-    if (project) throw new NotFoundException();
+    if (!project) throw new NotFoundException('Project not found');
     return project;
+  }
+
+  async create(createProjectDto: CreateProjectDto) {
+    const project = this.projectRepo.create(createProjectDto);
+    return await this.projectRepo.save(project);
+  }
+
+  async update(id: number, updateProjectDto: UpdateProjectDto) {
+    return await this.projectRepo.save({ ...updateProjectDto, id });
   }
 }
