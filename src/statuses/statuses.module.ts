0c15628094545ebabc8d82c85d291826d50fdc95
Islam Velikhanli <islamovice@gmail.com>
2026-03-04 15:47:25 +0400

    board fix

--- src/statuses/statuses.module.ts
+++ src/statuses/statuses.module.ts
@@ -3,10 +3,9 @@ import { StatusesService } from './statuses.service';
 import { StatusesController } from './statuses.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Status } from './status.entity';
-import { Board } from 'src/boards/boards.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Status, Board])],
+  imports: [TypeOrmModule.forFeature([Status])],
   controllers: [StatusesController],
   providers: [StatusesService],
 })
