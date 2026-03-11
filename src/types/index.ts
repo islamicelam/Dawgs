2317394416580b683bbdcd645c2e59954277e929
Islam Velikhanli <islamovice@gmail.com>
2026-03-11 18:04:26 +0400

    types and login api added

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+  }
+  
+  export interface Project {
+    id: number;
+    name: string;
+    description: string;
+    createdAt: string;
+  }
+  
+  export interface Board {
+    id: number;
+    name: string;
+    createdAt: string;
+  }
+  
+  export interface Status {
+    id: number;
+    name: string;
+    category: string;
+  }
+  
+  export interface Task {
+    id: number;
+    title: string;
+    description?: string;
+    status?: Status;
+    assign?: User;
+    createdAt: string;
+  }
