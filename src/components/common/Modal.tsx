d568f0e977f19eaf350b58a5abcdf8e8bbadb79e
Islam Velikhanli <islamovice@gmail.com>
2026-03-13 17:49:51 +0400

    onclose modal added

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -6,8 +6,16 @@ const Modal = ({
   onClose: () => void;
 }) => {
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
-      <div className="bg-white rounded-xl p-6 w-96 shadow-xl">{children}</div>
+    <div
+      className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white rounded-xl p-6 w-96 shadow-xl"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
     </div>
   );
 };
--- src/components/project/ProjectFormModal.tsx
+++ src/components/project/ProjectFormModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Modal from "../common/Modal";
 import type { Project } from "../../types";
 
